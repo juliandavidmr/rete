5a1210df577a268e7c90f75e2f4583f1175f9495
julian.mora <julian.mora@bizagi.com>
2018-12-17 10:40:26 -0500

    add support zoom to firefox

--- src/helpers/handlers.ts
+++ src/helpers/handlers.ts
@@ -1,6 +1,6 @@
 export module Handlers {
 
-    export function on(element: HTMLElement, events: string, handler: (...args: any[]) => void, options?: boolean | AddEventListenerOptions) {
+    export function on(element: HTMLElement, events: string, handler: (...args: any[]) => void, options: boolean | AddEventListenerOptions = false) {
         str2arr(events).map(eventName => {
             element.addEventListener(eventName, handler, options)
         })
@@ -13,16 +13,17 @@ export module Handlers {
             if (e.wheelDelta) {
                 return e.wheelDelta;
             }
-    
+
             if (e.originalEvent) {
                 if (e.originalEvent.detail) {
                     return e.originalEvent.detail * -40;
                 }
-        
+
                 if (e.originalEvent.wheelDelta) {
                     return e.originalEvent.wheelDelta;
                 }
             }
+            return Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail))); // works with both Chrome and FireFox
         }
     }
 }
